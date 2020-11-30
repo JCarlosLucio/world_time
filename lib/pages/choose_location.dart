00c29b4efaa7915df6d07bb0d0dbe2658e842a8e
jcLucio <c.lucio3@gmail.com>
2020-11-30 12:32:51 -0600

    Add CircleAvatar w/flag

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -32,10 +32,16 @@ class _ChooseLocationState extends State<ChooseLocation> {
       body: ListView.builder(
         itemCount: locations.length,
         itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              onTap: (){},
-              title: Text(locations[index].location),
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: (){},
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
             ),
           );
         },
