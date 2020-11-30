37c6871505c98b234934ae5430666fdfa6792524
jcLucio <c.lucio3@gmail.com>
2020-11-29 19:11:30 -0600

    Add String time to show loading/time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,10 +8,15 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  String time = 'loading';
+
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
@@ -23,7 +28,10 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
