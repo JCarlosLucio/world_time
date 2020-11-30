fb12eeb8acde610b3cf5b29dc84ba1a97801dffe
jcLucio <c.lucio3@gmail.com>
2020-11-29 19:06:45 -0600

    Refactor getTime() to WorldTime class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,30 +8,16 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    // // Response type / get() function given by http package
-    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
-    // // jsonDecode from convert package
-    Map data = jsonDecode(response.body);
-    // print(data);
-    // get properties from data
-    String datetime = data['datetime'];
-    // offset is the hours needed to reach the accurate time (+01:00)
-    // used substring to get '01'
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
-
-    // create DateTime object (comes with dart)
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
---
+++ lib/services/world_time.dart
@@ -0,0 +1,42 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for UI
+  String time; // time in that location for UI
+  String flag; // url to assets Flag Icon
+  String url; // location url for API endpoint
+
+  // constructor for named parameters
+  // used like this WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+  WorldTime({this.location, this.flag, this.url});
+
+  // Future type (works like JS promises)
+  Future<void> getTime() async {
+    // // Response type / get() function given by http package
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    // // jsonDecode from convert package
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    // offset is the hours needed to reach the accurate time (+01:00)
+    // used substring to get '01'
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object (comes with dart)
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set time property
+    time = now.toString();
+  }
+}
+
+
+
+
