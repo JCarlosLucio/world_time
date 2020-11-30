f6022cedf264bd01fb099cd7579fccbb3558276f
jcLucio <c.lucio3@gmail.com>
2020-11-30 11:30:01 -0600

    Add SpinKitFadingCube Widget

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -7,9 +8,9 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   void setupWorldTime() async {
-    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
     // replaces routes in routeStack with /home
     Navigator.pushReplacementNamed(context, '/home', arguments: {
@@ -28,10 +29,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube (
+          color: Colors.white,
+          size: 80.0,
+        )
+      )
     );
   }
 }
