992e0cfd531491bf359d2e7d1254536e67c12cf4
jcLucio <c.lucio3@gmail.com>
2020-11-30 09:36:50 -0600

    Add time Text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -18,29 +18,33 @@ class _HomeState extends State<Home> {
           child: Padding(
             padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: Column(
-        children: <Widget>[
-            FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit location')),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                FlatButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(Icons.edit_location),
+                    label: Text('Edit location')),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20.0),
                 Text(
-                  data['location'],
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    letterSpacing: 2.0,
-                  ),
-                )
+                    data['time'],
+                    style: TextStyle(fontSize: 66.0)),
               ],
-            )
-        ],
-      ),
-          )),
+            ),
+      )),
     );
   }
 }
